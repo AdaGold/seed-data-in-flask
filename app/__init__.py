8c468d96774b74c6404ee40fc5c68fa8f73919e0
Ansel Rognlie <36503186+anselrognlie@users.noreply.github.com>
2021-11-02 00:34:32 -0700

    Load connection string from .env

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -9,6 +10,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/seed_data_development'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/seed_data_development'
     app.config['SQLALCHEMY_ECHO'] = True
     app.url_map.strict_slashes = False
