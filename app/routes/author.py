df1ba9b9631489334bb387b037ecf28179aa7be6
Ansel Rognlie <36503186+anselrognlie@users.noreply.github.com>
2021-11-02 01:05:36 -0700

    Add route to get books for author

--- app/routes/author.py
+++ app/routes/author.py
@@ -9,3 +9,9 @@ def index():
     authors = Author.query.all()
     authors_response = [author.to_dict() for author in authors]
     return jsonify(authors_response)
+
+@bp.route("/<id>/books", methods=["GET"])
+def get_books(id):
+    author = Author.query.get(id)
+    books_response = [book.to_dict() for book in author.books]
+    return jsonify(books_response)
